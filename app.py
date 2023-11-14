4fa5e7cab9d7031c8af2ea71b20d5217db847a24
shettyshreyas-9 <shreyasshetty925@gmail.com>
2023-11-15 00:57:24 +0530

    Created the index.html template, recreated the venv for python==3.10 as pkl file from colab was not working due to pandas version, routed home to index,html and sent values from pkl to the resp. div. Created frame of recommend.html & downloaded pkl files for pt, book & simil_scores

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
+import pickle
+import pandas
 from flask import Flask, render_template
+
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+
+pt= pickle.load(open('pt.pkl','rb'))
+books= pickle.load(open('books.pkl','rb'))
+similarity_scores= pickle.load(open('similarity_scores.pkl','rb'))
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name = list(popular_df['Book-Title'].values),
+                           author = list(popular_df['Book-Author'].values),
+                           image = list(popular_df['Image-URL-M'].values),
+                           votes = list(popular_df['num_ratings'].values),
+                           rating = list(popular_df['avg_rating'].values)
+                            )
 
 
-
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
